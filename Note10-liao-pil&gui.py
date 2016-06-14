7751afe91bb80d9260ddab6ba9c3494ef1a2f125
dawnyoung <1991dongyangli@gmail.com>
2016-06-14 11:50:38 -0500

    no message

--- Note10-liao-pil&gui.py
+++ Note10-liao-pil&gui.py
@@ -107,11 +107,30 @@ class Application(Frame):
         self.helloLable.pack()
         self.quitButton = Button(self, text = 'quit', command = self.quit)
         self.quitButton.pack()
-    def hello(self):
-        name = self.nameInput.get() or 'world'
-        messagebox.showinfo('Message', 'hello %s' % name)
-        
+
 app = Application()
 app.master.title('hello world')
 app.mainloop()
 #%%)
+
+import tkinter
+import tkinter.messagebox as messagebox
+
+class Application2(Frame):
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alterButton = Button(self, text = 'hello', command = self.hello)
+        self.alterButton.pack()
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfor('Message', 'hello %s' % name)
+
+app = Application2()
+app.master.title('hello world')
+app.mainloop()
+#%%
